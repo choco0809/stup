aaa5f4f50e1456f9374c2d0a9094590a8aac5fdc
choco <superbeaver.180809@gmail.com>
2022-11-15 00:57:41 +0900

    slimファイルをTailwindcssで使用できるように変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
+    './app/views/**/*.html.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["cmyk"]
+  }
 }
