3f9461bed288a4ae4306ae986d5a357584dff08f
choco <superbeaver.180809@gmail.com>
2022-11-16 00:19:02 +0900

    Discord Web認証（OAuth）を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  post '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'log_out', to: 'sessions#destroy', as: 'log_out'
+  resources :sessions, only: %i[create destroy]
 end
