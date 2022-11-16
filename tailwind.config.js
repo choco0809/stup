8757cba34fa110e7538ef2b0c56c79aad1ce9b48
choco <superbeaver.180809@gmail.com>
2022-11-16 20:25:51 +0900

    tailwindcssに配色を追加した

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  theme: {
+    extend: {
+      colors: {
+        header: '#9ac5e5',
+        footer: '#edce7a'
+      }
+    }
+  },
   plugins: [require('daisyui')],
   daisyui: {
     themes: ["cmyk"]
