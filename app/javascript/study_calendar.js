ffb8e2cc5c07b4e699fa242af790863fb659cfc1
choco <superbeaver.180809@gmail.com>
2022-11-17 22:34:35 +0900

    ファイル名を変更した

--- app/javascript/study_calendar.js
+++ app/javascript/study_calendar.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import App from './calendar.vue'
+import App from './study_calendar.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  createApp(App).mount('#calendar')
+  createApp(App).mount('#study_calendar')
 })
