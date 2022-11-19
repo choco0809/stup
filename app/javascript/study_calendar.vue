98e116a44d93c12476035f82eac5b98f7c08478d
choco <superbeaver.180809@gmail.com>
2022-11-19 13:55:09 +0900

    カレンダーを遷移（前月、翌月）するためのボタンを作成

--- app/javascript/study_calendar.vue
+++ app/javascript/study_calendar.vue
@@ -1,19 +1,31 @@
 <template>
+  <div class="flex justify-center">
+    <div @click="previousMonth">
+      <button class="hover:border-b-2 border-black"> ＜ </button>
+    </div>
+    <div class="text-center mb-3 w-48">{{ calendarYear }} 年 {{ calendarMonth }} 月</div>
+    <div @click="nextMonth">
+      <button class="hover:border-b-2 border-black"> ＞ </button>
+    </div>
+  </div>
   <table class="w-full">
     <thead>
       <tr class="bg-base-200">
-        <th class = "border border-black w-1/7">日</th>
-        <th class = "border border-black w-1/7">月</th>
-        <th class = "border border-black w-1/7">火</th>
-        <th class = "border border-black w-1/7">水</th>
-        <th class = "border border-black w-1/7">木</th>
-        <th class = "border border-black w-1/7">金</th>
-        <th class = "border border-black w-1/7">土</th>
+        <th class="border border-black w-1/7">日</th>
+        <th class="border border-black w-1/7">月</th>
+        <th class="border border-black w-1/7">火</th>
+        <th class="border border-black w-1/7">水</th>
+        <th class="border border-black w-1/7">木</th>
+        <th class="border border-black w-1/7">金</th>
+        <th class="border border-black w-1/7">土</th>
       </tr>
     </thead>
     <tbody v-for="week in calendarWeeks" :key="week.id">
       <tr class="text-center">
-        <td v-for="date in week.value" :key="date.weekDay" class="border border-black h-12">
+        <td
+          v-for="date in week.value"
+          :key="date.weekDay"
+          class="border border-black h-12">
           {{ date.date }}
         </td>
       </tr>
@@ -40,7 +52,6 @@ export default {
         !date ? (date = { weekDay: weekDay }) : (date.weekDay = weekDay)
         value.push(date)
         weekDay++
-        // 7日事に配列をまとめてpushする
         if (value.length === 7 || i === ary.length - 1) {
           weekArry.push({ id: id, value: value })
           id++
@@ -48,28 +59,29 @@ export default {
           weekDay = 0
         }
       })
+      console.log(weekArry)
       return weekArry
     },
-    calendarDates(){
+    calendarDates() {
       const calendar = []
       if (this.firstWday > 0) {
         for (let blank = 0; blank < this.firstWday; blank++) {
           calendar.push(null)
         }
-        for (let date = 1; date <= this.lastDate; date++) {
-          calendar.push({ date: date })
-        }
+      }
+      for (let date = 1; date <= this.lastDate; date++) {
+        calendar.push({ date: date })
       }
       return calendar
     },
-    firstWday(){
+    firstWday() {
       const firstDay = new Date(this.calendarYear, this.calendarMonth - 1, 1)
       return firstDay.getDay()
     },
     lastDate() {
       const lastDay = new Date(this.calendarYear, this.calendarMonth, 0)
       return lastDay.getDate()
-    },
+    }
   },
   methods: {
     getCurrentYear() {
@@ -77,6 +89,22 @@ export default {
     },
     getCurrentMonth() {
       return new Date().getMonth() + 1
+    },
+    previousMonth() {
+      if (this.calendarMonth === 1){
+        this.calendarMonth = 12
+        this.calendarYear = this.calendarYear - 1
+      } else {
+        this.calendarMonth = this.calendarMonth - 1
+      }
+    },
+    nextMonth() {
+      if (this.calendarMonth === 12){
+        this.calendarMonth = 1
+        this.calendarYear = this.calendarYear + 1
+      } else {
+        this.calendarMonth = this.calendarMonth + 1
+      }
     }
   }
 }
