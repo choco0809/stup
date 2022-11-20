dc8362fb5cc6eb5e662b34ef9c33348a11a0c6b2
choco <superbeaver.180809@gmail.com>
2022-11-20 22:23:34 +0900

    tailwindcssにspacingを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,11 @@ module.exports = {
       colors: {
         header: '#9ac5e5',
         footer: '#edce7a'
+      },
+      spacing: {
+        '102' : '28rem',
+        '118' : '32rem',
+        '134' : '36rem'
       }
     }
   },
