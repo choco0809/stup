c40f580c7952d896dcf77b577a25cf338e120d5d
choco <superbeaver.180809@gmail.com>
2022-11-24 19:16:33 +0900

    study_time_recordテーブルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_070752) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_101045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "study_time_records", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "started_at"
+    t.datetime "{:null=>false}"
+    t.datetime "ended_at"
+    t.string "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_study_time_records_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "uid", null: false
@@ -21,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_070752) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "study_time_records", "users"
 end
