951345f9f8478a902baff8aaeb19de055f47763a
choco <superbeaver.180809@gmail.com>
2022-11-24 16:04:27 +0900

    usersテーブルのname、emailが不要なカラムのため削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_145543) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_070026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "uid"
-    t.string "name"
-    t.string "email"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
