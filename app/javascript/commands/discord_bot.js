1964481e6e6c034627a3b169a00c3a9ac55e739b
choco <superbeaver.180809@gmail.com>
2022-11-24 20:05:12 +0900

    discord_botを起動した際のログメッセージを変更

--- app/javascript/commands/discord_bot.js
+++ app/javascript/commands/discord_bot.js
@@ -2,7 +2,7 @@ const { Client, GatewayIntentBits } = require('discord.js')
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
 client.on('ready', () => {
-  console.log(`Discord Botを起動しました。`)
+  console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('interactionCreate', async (interaction) => {
