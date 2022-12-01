d159372d3b53b094fc58da778a5f6bc34a346a07
choco <superbeaver.180809@gmail.com>
2022-12-02 00:25:37 +0900

    前回の学習記録が終了しているか否かを判断するメソッドを作成

--- app/models/study_time_record.rb
+++ app/models/study_time_record.rb
@@ -2,4 +2,9 @@
 
 class StudyTimeRecord < ApplicationRecord
   belongs_to :user
+
+  def self.not_ended_at?(user)
+    records = where(user_id: user.id)
+    !records.empty? && records.last.ended_at.nil?
+  end
 end
