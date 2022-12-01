cec3d8ca1521444e4131adc1a0549e2461a9f8ac
choco <superbeaver.180809@gmail.com>
2022-12-01 23:10:38 +0900

    タイムゾーンを日本時間に固定

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,7 @@ module Stup
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
