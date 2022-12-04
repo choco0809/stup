100e272a9b235a12e3e086e721c58c86929c7828
choco <superbeaver.180809@gmail.com>
2022-12-05 00:45:27 +0900

    not_ended_at?メソッドをcontroller内に記述した

--- app/models/study_time_record.rb
+++ app/models/study_time_record.rb
@@ -2,9 +2,4 @@
 
 class StudyTimeRecord < ApplicationRecord
   belongs_to :user
-
-  def self.not_ended_at?(user)
-    records = where(user_id: user.id)
-    !records.empty? && records.last.ended_at.nil?
-  end
 end
