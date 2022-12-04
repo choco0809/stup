b7149556422d09198afff25a6963a6208213152a
choco <superbeaver.180809@gmail.com>
2022-12-05 00:07:12 +0900

    start、endコマンドを別ファイルに切り出した

--- app/javascript/commands/discord_bot.js
+++ app/javascript/commands/discord_bot.js
@@ -1,20 +1,13 @@
 const { Client, GatewayIntentBits } = require('discord.js')
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
-const start_command = require('./start_command.js')
+const startCommand = require('./start_command.js')
+const endCommand = require('./end_command.js')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-start_command(client)
+startCommand(client)
+endCommand(client)
 
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isChatInputCommand()) return
-
-  if (interaction.commandName === 'end') {
-    await interaction.reply('学習時間の記録を終了します。')
-  }
-})
-
-// client.login(process.env.DISCORD_BOT_TOKEN)
-client.login('MTAzMzMzNTQxNDU4MjQyMzU1Mg.GAT6Q5.5jCyOoKufmbiuzMlBvdUrdMyoRckzxsCAP7DcM')
+client.login(process.env.DISCORD_BOT_TOKEN)
