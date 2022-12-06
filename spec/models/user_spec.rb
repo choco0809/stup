9ace2bb1587e52fa8c194074e581d1f7fb58f8ec
choco <superbeaver.180809@gmail.com>
2022-12-06 23:09:09 +0900

    インスタンス変数の代わりにletを使用するよう変更した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Userモデルのテスト', type: :model do
+  let(:user) { FactoryBot.build(:user) }
+
   describe 'バリデーションのテスト' do
     it 'uidが存在する場合、バリエーションをパスする' do
-      user = FactoryBot.build(:user)
-      pp user
       expect(user).to be_valid
     end
 
     it 'uidが空の場合、バリデーションに失敗する' do
-      user = FactoryBot.build(:user, uid: nil)
-      pp user
+      user.uid = nil
       expect(user).to_not be_valid
     end
   end
