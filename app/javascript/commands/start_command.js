f6f22a16fa88bec8dd416d69bbab1636eea8acab
choco <superbeaver.180809@gmail.com>
2022-12-11 17:37:35 +0900

    URLを本番機用に修正

--- app/javascript/commands/start_command.js
+++ app/javascript/commands/start_command.js
@@ -6,7 +6,7 @@ function startCommand(client) {
 
     const nowDate = new Date()
     if (interaction.commandName === 'start') {
-      fetch('http://localhost:3000/api/discord/study_time_record', {
+      fetch('https://stup.fly.dev/api/discord/study_time_record', {
         method: 'POST',
         body: JSON.stringify({
           uid: interaction.user.id,
