85a1d408e3c8b2bc629ec37cf8135b249cc690fe
choco <superbeaver.180809@gmail.com>
2022-12-11 17:35:22 +0900

    改行を適切に追加、削除した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 RSpec.describe 'Userモデルのテスト', type: :model do
   describe 'バリデーションのテスト' do
     let(:user) { FactoryBot.build(:user) }
-
     context 'uidに値が入っている時' do
       it 'uidが存在する場合、バリエーションをパスする' do
         expect(user).to be_valid
@@ -40,12 +39,14 @@ RSpec.describe 'Userモデルのテスト', type: :model do
         expect { User.find_or_create_from_auth_hash(auth_hash) }.to change { User.count }.from(0).to(1)
       end
     end
+
     context 'uidに対応するUserが存在する場合' do
       let!(:created_user) { FactoryBot.create(:user, uid: @uid, image: @image) }
       it '引数と一致するUserレコードのオブジェクトが返されること' do
         user = User.find_or_create_from_auth_hash(auth_hash)
         expect(user).to eq created_user
       end
+
       it 'Userモデルのレコード件数が変化しないこと' do
         expect { User.find_or_create_from_auth_hash(auth_hash) }.not_to change(User, :count)
       end
