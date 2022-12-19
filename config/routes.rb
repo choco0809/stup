0c9a84d97842eb0457d62d95ffba3d4eb888d367
choco <superbeaver.180809@gmail.com>
2022-12-19 23:04:23 +0900

    resourceを追加 ※ resourceとして定義するのであれば単数系がふさわしいのでは？ 　要検討

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  get '/api/study_time_records', to: 'api/study_time_records#show'
   delete 'log_out', to: 'sessions#destroy'
   resources :sessions, only: %i[create destroy]
 
   namespace :api, {format: 'json'} do
+    resource :study_time_records, only: %i[show]
     namespace :discord, {format: 'json'} do
       resource :study_time_record, only: %i[create update]
     end
-    resources :study_time_records, only: %i[create update]
   end
 end
