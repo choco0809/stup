1187d7f71b648bafafc665568f394a443c1658d4
choco <superbeaver.180809@gmail.com>
2022-12-21 21:21:30 +0900

    リファクタリングを実施

--- app/controllers/api/discord/study_time_records_controller.rb
+++ app/controllers/api/discord/study_time_records_controller.rb
@@ -8,10 +8,10 @@ module Api
       def create
         return render status: :ok, json: { message: 'ユーザーが未登録です' } if @user.nil?
 
-        records = StudyTimeRecord.where(user_id: @user.id)
-        return render status: :ok, json: { message: '前回の学習記録が終了していません' } if check_ready_started?(records)
+        study_time_records = @user.study_time_records
+        return render status: :ok, json: { message: '前回の学習記録が終了していません' } if check_ready_started?(study_time_records.all)
 
-        study_time_record = records.new(started_at: params[:started_at])
+        study_time_record = study_time_records.new(started_at: params[:started_at])
         if study_time_record.save
           render status: :ok, json: { message: '学習の記録を開始しました' }
         else
@@ -22,11 +22,10 @@ module Api
       def update
         return render status: :ok, json: { message: 'ユーザーが未登録です' } if @user.nil?
 
-        records = StudyTimeRecord.where(user_id: @user.id).last
+        study_time_record = StudyTimeRecord.where(user_id: @user.id).last
+        return render status: :ok, json: { message: '学習が開始されていません' } if check_ready_ended?(study_time_record)
 
-        return render status: :ok, json: { message: '学習が開始されていません' } if check_ready_ended?(records)
-
-        if records.update(ended_at: params[:ended_at])
+        if study_time_record.update(ended_at: params[:ended_at])
           render status: :ok, json: { message: '学習が終了しました' }
         else
           render status: :ok, json: { message: 'エラーメッセージ' }
