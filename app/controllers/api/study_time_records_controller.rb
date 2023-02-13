e59d716ac31bb1b11a8f57788ffcfddfe4d1ddb4
choco <superbeaver.180809@gmail.com>
2023-02-13 23:58:08 +0900

    feat: :sparkles: updateメソッドを追加し、jsonを返すように変更した
    各項目はストロングパラメーターで更新するように変更

--- app/controllers/api/study_time_records_controller.rb
+++ app/controllers/api/study_time_records_controller.rb
@@ -4,17 +4,15 @@ module Api
   class StudyTimeRecordsController < ActionController::API
     include SessionsHelper
     before_action :set_user, only: %i[create destroy update]
+    before_action :set_study_time_record, only: %i[destroy update]
 
     def create
-      @study_time_record = @user.study_time_records.new(started_at: params[:started_at],
-                                                        ended_at: params[:ended_at],
-                                                        memo: params[:memo])
+      @study_time_record = @user.study_time_records.new(study_time_record_params)
       @study_time_record.save!
       @study_time_record
     end
 
     def destroy
-      @study_time_record = @user.study_time_records.find(params[:id])
       @study_time_record.destroy
       @study_time_record
     end
@@ -29,12 +27,24 @@ module Api
                             end
     end
 
-    def update; end
+    def update
+      @study_time_record.assign_attributes(study_time_record_params)
+      @study_time_record.save!
+      @study_time_record
+    end
 
     private
 
     def set_user
       @user = User.find(current_user.id)
+    end
+
+    def set_study_time_record
+      @study_time_record = @user.study_time_records.find(params[:id])
+    end
+
+    def study_time_record_params
+      params.require(:study_time_record).permit(:started_at, :ended_at, :memo)
     end
 
   end
