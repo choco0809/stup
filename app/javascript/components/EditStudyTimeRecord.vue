902b29743b0d20bb5113dfb6591ad84b1b00e2e3
choco <superbeaver.180809@gmail.com>
2023-02-15 00:27:03 +0900

    refactor: :recycle: インデントを2つに統一した

--- app/javascript/components/EditStudyTimeRecord.vue
+++ app/javascript/components/EditStudyTimeRecord.vue
@@ -4,26 +4,26 @@
       <div class="text-center w-1/3">開始時間</div>
       <div class="w-2/3">
         <VueTimepicker
-            id="startAt"
-            v-model="startedAtObject"
-            class="float-left"
-            name="startAt"
-            input-class="form-control"
-            manual-input
-            hide-dropdown />
+          id="startAt"
+          v-model="startedAtObject"
+          class="float-left"
+          name="startAt"
+          input-class="form-control"
+          manual-input
+          hide-dropdown />
       </div>
     </div>
     <div class="flex justify p-2">
       <div class="text-center w-1/3">終了時間</div>
       <div class="w-2/3">
         <VueTimepicker
-            id="endAt"
-            v-model="endedAtObject"
-            class="float-left"
-            name="endAt"
-            input-class="form-control"
-            manual-input
-            hide-dropdown />
+          id="endAt"
+          v-model="endedAtObject"
+          class="float-left"
+          name="endAt"
+          input-class="form-control"
+          manual-input
+          hide-dropdown />
       </div>
     </div>
     <div class="flex justify-center p-2">
