dcdba6778fa474d0c9d6f1558e3bdc613c4b8287
choco <superbeaver.180809@gmail.com>
2023-04-12 15:55:23 +0900

    feat :+1: 棒グラフに学習時間を反映させた
    学習カレンダーの学習時間と同期して棒グラフのデータも変更される

--- app/javascript/components/ChartVar.vue
+++ app/javascript/components/ChartVar.vue
@@ -2,97 +2,126 @@
   <canvas id="myChart"></canvas>
 </template>
 <script>
-import { Chart, LinearScale, CategoryScale, BarController, BarElement, UpdateMode } from 'chart.js'
-import {watch, onMounted, shallowRef} from 'vue'
-import {useStore} from 'vuex'
+import {
+  Chart,
+  LinearScale,
+  CategoryScale,
+  BarController,
+  BarElement
+} from 'chart.js'
+import { watch, onMounted, shallowRef } from 'vue'
+import { useStore } from 'vuex'
+import useStudyTimeRecordFunction from './functions/UseStudyTimeRecordFunction.vue'
 Chart.register(LinearScale, CategoryScale, BarController, BarElement)
 
 export default {
-  setup(){
+  setup() {
     const store = useStore()
+    const { formatDay } = useStudyTimeRecordFunction()
     let barChart = shallowRef()
 
     const renderChart = (labels) => {
-      const canvas = document.getElementById("myChart")
+      const canvas = document.getElementById('myChart')
       if (canvas === null) return
-      const ctx = canvas.getContext("2d")
-      barChart = shallowRef(new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: labels,
-          datasets: [{
-            label: '# of Votes',
-            data: [10],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-              grid: {
-                display: false
+      const ctx = canvas.getContext('2d')
+      barChart = shallowRef(
+        new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels,
+            datasets: [
+              {
+                label: '# of Votes',
+                data: [],
+                borderWidth: 1
               }
-            },
-            x: {
-              ticks: {
-                autoSkip: false,
-                maxRotation: 0,
-                minRotation: 0,
+            ]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+                grid: {
+                  display: false
+                }
               },
-              grid: {
-                display: false
+              x: {
+                ticks: {
+                  autoSkip: false,
+                  maxRotation: 0,
+                  minRotation: 0
+                },
+                grid: {
+                  display: false
+                }
               }
             }
           }
-        }
-      }))
+        })
+      )
     }
 
     const createMonthlyCalendar = () => {
       const monthlyCalendar = []
       const year = store.getters.calendarYear
       const month = store.getters.calendarMonth
-      const endOfMonth = new Date(year, month , 0).getDate()
-      for(let i = 1; i <= 31; i++ ) {
+      const endOfMonth = new Date(year, month, 0).getDate()
+      for (let i = 1; i <= 31; i++) {
         if (i <= endOfMonth) {
           monthlyCalendar.push(i)
         } else {
           monthlyCalendar.push(null)
         }
-
       }
       return monthlyCalendar
     }
 
-    onMounted( () => {
+    onMounted(() => {
       const label = createMonthlyCalendar()
       renderChart(label)
-    } )
+    })
 
-    const deleteLabelSets = () => {
-      let bar = barChart.value
-      const max = bar.data.labels.length
-      for(let i=0; i < max; i++){
-        bar.data.labels.pop()
-      }
-      bar.update()
+    const createDataSets = () => {
+      const monthlyCalendar = createMonthlyCalendar()
+      const monthlyStudyTime = store.getters.monthlyStudyTime
+      const dataSets = monthlyCalendar.map((index) => {
+        const totalDailyStudyTime = monthlyStudyTime.filter((record) => {
+          return record.started_at.substring(8, 10).includes(formatDay(index))
+        })
+        if (totalDailyStudyTime.length) {
+          return totalDailyStudyTime.reduce(function (sum, records) {
+            if (records.ended_at == null) {
+              return sum + 0
+            } else {
+              const startedAt = new Date(records.started_at)
+              const endedAt = new Date(records.ended_at)
+              const studyTime = Math.floor((endedAt - startedAt) / 1000 / 60)
+              return sum + studyTime
+            }
+          }, 0)
+        } else {
+          return 0
+        }
+      })
+      return dataSets
     }
 
-    watch( () => store.getters.calendarMonth, () => {
-      let bar = barChart.value
-      const label = createMonthlyCalendar()
-      deleteLabelSets()
-      label.forEach((elem) => {
-        bar.data.labels.push(elem)
-      })
-      bar.update()
-    }, {deep: true})
+    watch(
+      () => store.getters.monthlyStudyTime,
+      () => {
+        const chart = barChart.value
+        const label = createMonthlyCalendar()
+        const dataSets = createDataSets()
+        chart.data.labels = label
+        chart.data.datasets[0].data = dataSets
+        chart.update('none')
+      },
+      { deep: true }
+    )
 
     return {
-      barChart,
+      barChart
     }
   }
 }
-
 </script>
