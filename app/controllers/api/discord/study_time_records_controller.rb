f5bd634983bb2b6a60824e656fd1e3a711e9debf
choco <superbeaver.180809@gmail.com>
2023-04-24 14:37:25 +0900

    feat :+1: slash commandを実行したときの文言を変更

--- app/controllers/api/discord/study_time_records_controller.rb
+++ app/controllers/api/discord/study_time_records_controller.rb
@@ -5,31 +5,32 @@ module Api
     class StudyTimeRecordsController < ActionController::API
       before_action :set_user
 
+      STUP_URL = 'https://stup.fly.dev/'
+      UNREGISTERED_USER = "ユーザーが登録されていません。\n下記のURLからユーザー登録を行った後、再度実行して下さい🙇\n#{STUP_URL}".freeze
+      START_REPORT = '学習の記録を開始しました🙆'
+      FINISH_REPORT = '学習が終了しました🙆'
+      INCOMPLETE_REPORT = '前回の学習記録が終了していません🙅'
+      NOT_STARTED = "学習が開始されていません。\n学習の記録を開始してください🙇"
+
       def create
-        return render status: :ok, json: { message: 'ユーザーが未登録です' } if @user.nil?
+        return render status: :ok, json: { message: UNREGISTERED_USER } if @user.nil?
 
         study_time_records = @user.study_time_records
-        return render status: :ok, json: { message: '前回の学習記録が終了していません' } if check_ready_started?(study_time_records.all)
+        return render status: :ok, json: { message: INCOMPLETE_REPORT } if check_ready_started?(study_time_records.all)
 
         study_time_record = study_time_records.new(started_at: params[:started_at], memo: params[:memo])
-        if study_time_record.save
-          render status: :ok, json: { message: '学習の記録を開始しました' }
-        else
-          render status: :ok, json: { message: 'エラーメッセージ' }
-        end
+        study_time_record.save!
+        render status: :ok, json: { message: START_REPORT }
       end
 
       def update
-        return render status: :ok, json: { message: 'ユーザーが未登録です' } if @user.nil?
+        return render status: :ok, json: { message: UNREGISTERED_USER } if @user.nil?
 
         study_time_record = StudyTimeRecord.where(user_id: @user.id).last
-        return render status: :ok, json: { message: '学習が開始されていません' } if check_ready_ended?(study_time_record)
+        return render status: :ok, json: { message: NOT_STARTED } if check_ready_ended?(study_time_record)
 
-        if study_time_record.update(ended_at: params[:ended_at])
-          render status: :ok, json: { message: '学習が終了しました' }
-        else
-          render status: :ok, json: { message: 'エラーメッセージ' }
-        end
+        study_time_record.update!(ended_at: params[:ended_at])
+        render status: :ok, json: { message: FINISH_REPORT }
       end
 
       private
