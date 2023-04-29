d63e102d5c88a4ece92177af4fc7d94916067bf7
choco <superbeaver.180809@gmail.com>
2023-04-29 13:40:39 +0900

    refactor :recycle: メソッドを作成せずにコードを実行していたため、適切なメソッドを作成

--- app/javascript/commands/discord_bot.js
+++ app/javascript/commands/discord_bot.js
@@ -1,13 +1,22 @@
 const { Client, GatewayIntentBits } = require('discord.js')
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
-const startCommand = require('./start_command.js')
-const endCommand = require('./end_command.js')
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+const setupCommands = (client) => {
+  const startCommand = require('./start_command.js')
+  const endCommand = require('./end_command.js')
 
-startCommand(client)
-endCommand(client)
+  startCommand(client)
+  endCommand(client)
+}
 
-client.login(process.env.DISCORD_BOT_TOKEN)
+const runBot = () => {
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+  })
+
+  setupCommands(client)
+  client.login(process.env.DISCORD_BOT_TOKEN)
+}
+
+runBot()
