c96b47aa72ccfc1a9f117677be6f83c5b314dc3c
choco <superbeaver.180809@gmail.com>
2023-04-29 11:26:55 +0900

    refactor :recycle: リプライメッセージを定数化

--- spec/controllers/api/discord/study_time_records_controller_spec.rb
+++ spec/controllers/api/discord/study_time_records_controller_spec.rb
@@ -3,6 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Api::Discord::StudyTimeRecordsController, type: :controller do
+
+  STUP_URL = 'https://stup.fly.dev/'
+  UNREGISTERED_USER = "ユーザーが登録されていません。\n下記のURLからユーザー登録を行った後、再度実行して下さい🙇\n#{STUP_URL}".freeze
+  START_REPORT = '学習の記録を開始しました🙆'
+  FINISH_REPORT = '学習が終了しました🙆'
+  INCOMPLETE_REPORT = '前回の学習記録が終了していません🙅'
+  NOT_STARTED = "学習が開始されていません。\n学習の記録を開始してください🙇"
+  OVER_24_HOURS = "前回の学習記録から24時間以上経過しています。\n下記のURLから手動にて終了時間を記入して下さい🙇\n#{STUP_URL}".freeze
+
   describe 'create' do
     let(:user) { FactoryBot.build(:user, uid: '1234567') }
     context 'ユーザーが未登録の場合' do
@@ -12,7 +21,7 @@ RSpec.describe Api::Discord::StudyTimeRecordsController, type: :controller do
           started_at: '2022/12/11 15:00:00'
         }
         expect(response).to have_http_status '200'
-        expect(response.parsed_body['message']).to eq "ユーザーが登録されていません。\n下記のURLからユーザー登録を行った後、再度実行して下さい🙇\nhttps://stup.fly.dev/"
+        expect(response.parsed_body['message']).to eq UNREGISTERED_USER
         expect(StudyTimeRecord.count).to eq 0
       end
     end
@@ -26,7 +35,7 @@ RSpec.describe Api::Discord::StudyTimeRecordsController, type: :controller do
         }
         record = StudyTimeRecord.find_by(user_id: user.id)
         expect(response).to have_http_status '200'
-        expect(response.parsed_body['message']).to eq '学習の記録を開始しました🙆'
+        expect(response.parsed_body['message']).to eq START_REPORT
         expect(StudyTimeRecord.find(record.id).started_at).to eq '2022-12-11 15:00:00.000000000 +0900'
         expect(StudyTimeRecord.count).to eq 1
       end
@@ -45,7 +54,7 @@ RSpec.describe Api::Discord::StudyTimeRecordsController, type: :controller do
         }
 
         expect(response).to have_http_status '200'
-        expect(response.parsed_body['message']).to eq "前回の学習記録が終了していません🙅\n2022/12/11 15:00"
+        expect(response.parsed_body['message']).to eq "#{INCOMPLETE_REPORT}\n2022/12/11 15:00"
         expect(StudyTimeRecord.count).to eq 1
       end
     end
@@ -60,7 +69,7 @@ RSpec.describe Api::Discord::StudyTimeRecordsController, type: :controller do
           ended_at: '2022/12/11 15:00:00'
         }
         expect(response).to have_http_status '200'
-        expect(response.parsed_body['message']).to eq "ユーザーが登録されていません。\n下記のURLからユーザー登録を行った後、再度実行して下さい🙇\nhttps://stup.fly.dev/"
+        expect(response.parsed_body['message']).to eq UNREGISTERED_USER
         expect(StudyTimeRecord.count).to eq 0
       end
     end
@@ -73,7 +82,7 @@ RSpec.describe Api::Discord::StudyTimeRecordsController, type: :controller do
           ended_at: '2022/12/11 15:00:00'
         }
         expect(response).to have_http_status '200'
-        expect(response.parsed_body['message']).to eq "学習が開始されていません。\n学習の記録を開始してください🙇"
+        expect(response.parsed_body['message']).to eq NOT_STARTED
         expect(StudyTimeRecord.count).to eq 0
       end
 
@@ -86,7 +95,7 @@ RSpec.describe Api::Discord::StudyTimeRecordsController, type: :controller do
         record = StudyTimeRecord.find_by(user_id: user.id)
 
         expect(response).to have_http_status '200'
-        expect(response.parsed_body['message']).to eq '学習の記録を開始しました🙆'
+        expect(response.parsed_body['message']).to eq START_REPORT
 
         # 学習記録を終了する
         patch :update, params: {
@@ -95,7 +104,7 @@ RSpec.describe Api::Discord::StudyTimeRecordsController, type: :controller do
         }
 
         expect(response).to have_http_status '200'
-        expect(response.parsed_body['message']).to eq '学習が終了しました🙆'
+        expect(response.parsed_body['message']).to eq FINISH_REPORT
         expect(StudyTimeRecord.find(record.id).ended_at).to eq '2022-12-11 16:00:00.000000000 +0900'
       end
     end
